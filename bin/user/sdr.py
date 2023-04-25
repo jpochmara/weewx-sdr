33330a56fac7d0e5b3c628ac0a91d907cad92e12
John Pochmara <127617714+jpochmara@users.noreply.github.com>
2023-04-25 09:43:04 -0700

    Update sdr.py
    Added in Freq to WH40.
    Changed shutdown to use terminate() instead of kill().

--- bin/user/sdr.py
+++ bin/user/sdr.py
@@ -241,7 +241,7 @@ class ProcManager(object):
 
     def shutdown(self):
         loginf('shutdown process %s' % self._cmd)
-        self._process.kill()
+        self._process.terminate()
         logdbg("close stdout")
         self._process.stdout.close()
         logdbg("close stderr")
@@ -1190,6 +1190,8 @@ class EcoWittWH40Packet(Packet):
         pkt['rain_total'] = Packet.get_float(obj, 'rain_mm')
         pkt['battery'] = 0 if Packet.get_int(obj, 'battery_ok') > 0.10 else 1
         pkt['supplyVoltage'] = Packet.get_float(obj, 'battery_V')
+        pkt['freq1'] = Packet.get_float(obj, 'freq1')
+        pkt['freq2'] = Packet.get_float(obj, 'freq2')
         pkt['rssi'] = Packet.get_float(obj, 'rssi')
         pkt['snr'] = Packet.get_float(obj, 'snr')
         pkt['noise'] = Packet.get_float(obj, 'noise')
